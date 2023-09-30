dd03acabdee37fec597756b4f0d68f6e1336eb00
datttrian <datttrian@gmail.com>
2023-09-30 14:13:59 -0700

    Add utils extractCurrency and extractDescription

--- lib/utils.ts
+++ lib/utils.ts
@@ -17,3 +17,33 @@ export function extractPrice(...elements: any) {
 
   return "";
 }
+
+// Extracts and returns the currency symbol from an element.
+export function extractCurrency(element: any) {
+  const currencyText = element.text().trim().slice(0, 1);
+  return currencyText ? currencyText : "";
+}
+
+// Extracts description from two possible elements from amazon
+export function extractDescription($: any) {
+  // these are possible elements holding description of the product
+  const selectors = [
+    ".a-unordered-list .a-list-item",
+    ".a-expander-content p",
+    // Add more selectors here if needed
+  ];
+
+  for (const selector of selectors) {
+    const elements = $(selector);
+    if (elements.length > 0) {
+      const textContent = elements
+        .map((_: any, element: any) => $(element).text().trim())
+        .get()
+        .join("\n");
+      return textContent;
+    }
+  }
+
+  // If no matching elements were found, return an empty string
+  return "";
+}
