2be6431aa80032db57cb49750ecfc4e069a79641
datttrian <datttrian@gmail.com>
2023-09-30 15:34:31 -0700

    Add getAllProducts function to retrieve all products

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -57,3 +57,15 @@ export async function getProductById(productId: string) {
     return product;
   } catch (error) {}
 }
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
+}
